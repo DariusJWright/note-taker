7cfbeb07a2c86aa5639ac258ef9d8a9cd7234c6e
darshnablah <dwright758@gmail.com>
2020-08-02 16:06:53 -0500

    saving notes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require('./db/db.json');
+const notes = require('./db/db.json');
 
 console.log(notes);
 
